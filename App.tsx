9f49a67a3da80e92fd95ec4f960a95336e67e44c
Grant Wilkinson <grant@aimermedia.com>
2024-02-10 11:51:40 +0000

    Add form logic

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 
 import { useState } from 'react'
 import styled from 'styled-components'
-import { Text, View, FlatList,  } from 'react-native'
+import { Text, View, FlatList, NativeSyntheticEvent, TextInputChangeEventData, Button } from 'react-native'
 
 import { allQuotes } from './mock-data/allQuotes'
 import QuoteCard from './components/QuoteCard'
+import Form from './components/Form'
+import { QuoteData } from './App.interface'
 
 const Page = styled(View)`
   flex: 1;
@@ -21,10 +23,30 @@ const Heading = styled(Text)`
 `
 
 const App = () => {
-  const [allCurrentQuotes, setAllCurrentQuotes] = useState(allQuotes)
+  const [allCurrentQuotes, setAllCurrentQuotes] = useState<QuoteData[]>(allQuotes)
+  const [showForm, setShowForm] = useState<boolean>(false)
+  const [formValues, setFormValues] = useState<QuoteData>({
+    movie: '',
+    quote: '',
+    createdBy: '',
+    upvotes: 0
+  })
+
+  const handleFormChange = (name: string, value: string) => {
+    setFormValues({ ...formValues, [name]: value })
+  }
+
+  const saveQuote = () => {
+    setAllCurrentQuotes([...allCurrentQuotes, formValues])
+  }
+
   return (
     <Page>
       <Heading>My Favourite Movie Quotes</Heading>
+      <Button title={`${showForm ? 'Close' : 'Open'} new quote form`} onPress={() => setShowForm(!showForm)} />
+      {showForm ? (
+        <Form formValues={formValues} handleFormChange={handleFormChange} saveQuote={saveQuote} />
+      ) : null}
       <FlatList
         data={allCurrentQuotes}
         renderItem={({item}) => <QuoteCard quoteData={item} />}
