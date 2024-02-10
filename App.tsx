206400c87f1a66ecb1cd53fc37703740eadce35d
Grant Wilkinson <grant@aimermedia.com>
2024-02-10 10:10:21 +0000

    boilerplate-and-basic-layout

--- App.tsx
+++ App.tsx
@@ -1,18 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import { useState } from 'react'
+import styled from 'styled-components'
+import { Text, View, FlatList } from 'react-native'
+
+import { allQuotes } from './mock-data/allQuotes'
+
+const Page = styled(View)`
+  flex: 1;
+  backgroundColor: #eee;
+  alignItems: center;
+  justifyContent: center;
+  padding-vertical: 60px;
+  padding-horizontal: 16px;
+`
+
+const Heading = styled(Text)`
+  padding: 8px;
+  font-size: 24px;
+`
+
+const Card = styled(View)`
+  font-size: 18px;
+  border-radius: 8px;
+  backgroundColor: #fff;
+  padding: 16px;
+  margin-vertical: 8px;
+`
+
+const Item = ({quote}) => (
+  <Card>
+    <Text>{quote}</Text>
+  </Card>
+)
+
+const App = () => {
+  const [allCurrentQuotes, setAllCurrentQuotes] = useState(allQuotes)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+    <Page>
+      <Heading>My Favourite Movie Quotes</Heading>
+      <FlatList
+        data={allCurrentQuotes}
+        renderItem={({item}) => <Item quote={item.quote} />}
+        keyExtractor={item => item.quote}
+      />
+    </Page>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
---
+++ mock-data/allQuotes.ts
@@ -0,0 +1,39 @@
+
+export const allQuotes = [
+  {
+    quote: 'E.T go home',
+    movie: 'E.T',
+    createdBy: 'Jim',
+    upvotes: 3
+  },
+  {
+    quote: 'I\'\ll make him an offer he can\'\t refuse',
+    movie: 'The Godfather',
+    createdBy: 'Bob',
+    upvotes: 22
+  },
+  {
+    quote: 'Hello World',
+    movie: 'Some Movie',
+    createdBy: 'Jim',
+    upvotes: 0
+  },
+  {
+    quote: 'You talkin to me?',
+    movie: 'Taxi driver',
+    createdBy: 'Jim',
+    upvotes: 62
+  },
+  {
+    quote: 'Get down!!',
+    movie: 'Terminator',
+    createdBy: 'Bob',
+    upvotes: 17
+  },
+  {
+    quote: 'Falling with style',
+    movie: 'Toy Story',
+    createdBy: 'Jim',
+    upvotes: 4
+  }
+]
